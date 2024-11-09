cd883b7517675b7651f22ce66d1046bcd10b4fc8
Akbar Rahmani <a.rahmanii@sarmayex.com>
2024-11-09 21:46:39 +0330

    types

---
+++ types.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$string = "my name is ramtin";
+
+$string2 = 'سلام چطوری';
+
+$int = 1373;
+
+$intType2 = -12000;
+
+$floatType = 30.12;
+
+$array = [
+    'akbar', 'rahmani', 1373, 200.300
+];
+
+
+var_dump($string);
+var_dump($string2);
+var_dump($int);
+var_dump($intType2);
+var_dump($floatType);
+var_dump($array);
+
+echo "php types";
+echo "<br>";
+echo 'end';
+

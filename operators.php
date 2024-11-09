1366edaeadb8e8000b980fed0c34903a37e40bcd
Akbar Rahmani <a.rahmanii@sarmayex.com>
2024-11-09 21:57:57 +0330

    simple operators

---
+++ operators.php
@@ -0,0 +1,43 @@
+<?php
+
+echo 'تفریق';
+echo '<br>';
+echo 10 - 8;
+echo '<br>';
+
+echo 'جمع';
+echo '<br>';
+echo 10 + 8;
+echo '<br>';
+
+echo 'ضرب';
+echo '<br>';
+echo 10 * 8;
+echo '<br>';
+
+echo 'تقسیم';
+echo '<br>';
+echo 10 / 8;
+echo '<br>';
+
+echo 'باقی مانده';
+echo '<br>';
+echo 10 % 8;
+echo '<br>';
+
+echo 'توان';
+echo '<br>';
+echo 10 ** 8;
+
+
+var_dump(1 == 2); // ایا مساوی هستن‌؟ خیر
+var_dump(1 == 1); // ایا مساوی هستن ؟ بله
+var_dump('akbar' == 'akabr rahmani'); // ایا برابر هستن ؟ خیر
+var_dump('akbar' == 'akbar'); // ایا برابر هستن ؟ بله
+
+var_dump('akbar' != 'rahmani'); // ایا برابر نیستن ؟ بله
+var_dump('akbar' != 'akbar'); // ایا برابر نیستن ؟ خیر
+
+var_dump(true === false); // ایا یکی هستن ؟ خیر - به همراه نوع
+
+
